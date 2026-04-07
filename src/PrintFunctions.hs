1b9ed0a28a2164f428b8ff27f10a4810e66c801d
Martin Elde Knutsen <maknu0536@uib.no>
2026-04-07 15:04:43 +0200

    Fikset importet puttet de på linje

--- src/PrintFunctions.hs
+++ src/PrintFunctions.hs
@@ -1,12 +1,17 @@
-
 module PrintFunctions (printResults ) where
-import TraverselsFunctions ( DirContent ) 
-import qualified Data.ByteString.Char8 as BS(unpack )
-import System.Posix.Directory.Foreign (dtLnk,dtDir )
+
+
+import TraverselsFunctions                   (DirContent) 
+import qualified Data.ByteString.Char8 as BS (unpack)
+import System.Posix.Directory.Foreign        (dtLnk, dtDir)
+
 import System.Console.ANSI
-
-
--- | Printer resultatet, bruker linebuffer slik at søkeresultaene skal bli synlig forløpende
+    ( setSGR,
+      Color(Red),
+      ColorIntensity(Vivid),
+      ConsoleLayer(Foreground),
+      SGR(Reset, SetColor) 
+      )
 
 printResults :: [DirContent] -> IO ()
 printResults contents = do
@@ -14,11 +19,9 @@ printResults contents = do
     mapM_ printDirContent $ filter ((/= dtDir ) . fst) contents 
     setSGR [Reset]
 
-
 -- | printer et Enkelt DirContent element
 printDirContent :: DirContent -> IO ()
 printDirContent = putStrLn . BS.unpack . snd   
-
 
 -- | printer et Enkelt DirContent element
 -- | Printer men legger til / for mappe
