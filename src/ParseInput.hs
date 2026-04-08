a45ab5a1e16239cb54038c1ea34de27e1d413f37
Martin Elde Knutsen <maknu0536@uib.no>
2026-04-08 12:37:30 +0200

    Skrevet ferdig parser for alle flaggene. Borsett fra execute, den ser vi på senere

--- src/ParseInput.hs
+++ src/ParseInput.hs
@@ -1,8 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS_GHC -Wno-unused-do-bind #-}
+{- HLINT ignore "Use <$>" -}
+
 module ParseInput where
 
-import TraversalSettings    ( SearchSetting (..), FilterFlags (..))
+import TraversalSettings    ( SearchSetting (..), FilterFlags (..), convertString)
 import Data.Void            (Void)
 import Control.Applicative  ((<|>))
 import Data.Functor         (($>))
@@ -17,7 +19,8 @@ import Text.Megaparsec (
     , manyTill
     , eof
     , lookAhead
-    , choice )
+    , choice, skipMany )
+import Data.List (foldl')
  
 type Parser = Parsec Void String
 
@@ -26,49 +29,87 @@ tester = do
     test <- getLine 
     parseTest parseLamdaSearch test 
 
+
+-- Parser helper
 parseSpace :: Parser Char
 parseSpace = char ' '
 
 parseLms :: Parser String
 parseLms = string "lms "
 
+sc :: Parser ()
+sc = skipMany (char ' ' <|> char '\t')
+
+parseUntilSpace :: Parser String 
+parseUntilSpace = many (satisfy (/=' '))
+
+
+emptyFilterFlags :: FilterFlags
+emptyFilterFlags = FilterFlags {
+      regxPattern = Nothing
+    , exclude     = Nothing
+    , extention   = Nothing
+    , hideHidden  = False
+    } 
+
+
 data DataFlags = 
-      SearchPatternFlag 
+      SearchPatternFlag String
     | HiddenFilesFlag
-    | ExtentionFlag
-    | IgnoreFlag
-    | ExecuteFlag
+    | ExtentionFlag     String
+    | IgnoreFlag        [String]
+    | ExecuteFlag       String
+
 
 pFlags :: Parser DataFlags
 pFlags = choice
-    [  SearchPatternFlag <$ (string "-p" <|> string  "--pattern")
-     , HiddenFilesFlag   <$ (string "-a" <|> string  "--show--dots")
-     , ExtentionFlag     <$ (string "-e" <|> string  "--extention")
-     , IgnoreFlag        <$ (string "-i" <|> string  "--ignore")
-     , ExecuteFlag       <$ (string "-x" <|> string  "--execute")
+    [  SearchPatternFlag <$> ((string "-p" <|> string  "--pattern")    *> sc *> parseUntilSpace)
+     , HiddenFilesFlag   <$  ( string "-a" <|> string  "--show--dots")
+     , ExtentionFlag     <$> ((string "-e" <|> string  "--extention")  *> sc *> parseUntilSpace)
+     , IgnoreFlag        <$> ((string "-i" <|> string  "--ignore")     *> sc *> pathsUntilFlag)
+     , ExecuteFlag       <$> ((string "-x" <|> string  "--execute")    *> sc *> parseUntilSpace) --skipper forløpign
     ]
 
+
+
+applyFlag :: FilterFlags -> DataFlags -> FilterFlags
+applyFlag st df = case df of
+     SearchPatternFlag s -> st {regxPattern = Just (convertString s)}
+     ExtentionFlag     e -> st {extention   = Just (convertString  e)}
+     IgnoreFlag        i -> st {exclude     = Just (map convertString i)}
+     HiddenFilesFlag     -> st {hideHidden  = True}
+     ExecuteFlag       _ -> st
+
+parseAllFlags :: Parser [DataFlags]
+parseAllFlags = many (sc *> pFlags <* sc)
+
+-- Satan for en eleganse i dette. Hadde tenkt å bruke state monaden, men trenger ikke det
+parseFlags :: Parser FilterFlags
+parseFlags = do
+  fs <- parseAllFlags --Løfter ut 
+  pure (foldl' applyFlag emptyFilterFlags fs)  --foldr over
+
+-- parser for stier
 parsePath :: Parser String
 parsePath = between (char '"') (char '"') (many (satisfy (/= '"')))
 
 pathsUntilFlag :: Parser [String]
 pathsUntilFlag =
   manyTill
-    (parsePath <* many (char ' ') )
+    (parsePath <* sc)
     (lookAhead (pFlags $> () <|> eof))
 
-
-
+-- Main parser
 parseLamdaSearch :: Parser SearchSetting
 parseLamdaSearch = do 
     parseLms 
     paths <- pathsUntilFlag 
+    flags <- parseFlags 
     pure $ SearchSetting 
-        {
-          searchPaths     = paths
+        { 
+           searchPaths    = paths
          , applyedCommand = Nothing
-         , filters        = Nothing
-
+         , filters        = Just flags
         }
     
 
