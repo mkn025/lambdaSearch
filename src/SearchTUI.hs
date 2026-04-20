0be45877dbeb255d0ea8d05b85587253ec020b85
Martin Elde Knutsen <maknu0536@uib.no>
2026-04-20 14:59:12 +0200

    Add dependencies

--- src/SearchTUI.hs
+++ src/SearchTUI.hs
@@ -1,3 +1,35 @@
-module PrintFunctions where
+{-# LANGUAGE TemplateHaskell #-}
 
+import Brick
+import Lens.Micro ()
+import Brick.Widgets.Edit (Editor)
+import Data.Text (Text)
+
+data MyState n = MyState { _editor :: Editor Text n }
+
+makeLenses ''MyState
+
+
+main :: IO ()
+main = simpleMain ui
+
+
+ui :: Widget ()
+ui = str "Hello, world!"
+
+
+
+data App s e n = App {
+         appDraw         :: s -> [Widget n]
+        , appChooseCursor :: s -> [CursorLocation n] -> Maybe (CursorLocation n)
+        , appHandleEvent  :: BrickEvent n e -> EventM n s ()
+        , appStartEvent   :: EventM n s ()
+        , appAttrMap      :: s -> AttrMap
+        }
+
+
+
+
+handleEvent :: BrickEvent n e -> EventM n s () -- skal 
+handleEvent = undefined 
 
