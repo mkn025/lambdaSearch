4a2c8a06926d983dcd0688d869d8576a98f5d641
Martin Elde Knutsen <maknu0536@uib.no>
2026-04-22 13:13:18 +0200

    Refactored main

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,8 @@
 module Main where
 
-import System.Environment  (getArgs )
-
+import SearchTUI           (mainTUI)
 import ParseInput          (runParserIO)
+import System.Environment  (getArgs )
 
 import TraverselsFunctions (treverseDirWithSettings)
 import PrintFunctions      (printResults)
@@ -10,12 +10,32 @@ import System.IO           (stdout, BufferMode(LineBuffering), hSetBuffering )
 
 main :: IO ()
 main = do
-     hSetBuffering stdout LineBuffering 
-     input  <- getArgs 
-     let inpString =  concatMap ( ' ' :) input
+     hSetBuffering stdout LineBuffering
+     input  <- getArgs
+     actOnInput input
+
+
+runTUI :: IO ()
+runTUI = mainTUI
+
+runCLi :: [String] -> IO ()
+runCLi args = do
+     let inpString =  concatMap ( ' ' :) args
      ss     <- runParserIO inpString
      output <- treverseDirWithSettings ss
      printResults output
+
+actOnInput :: [String] ->  IO()
+actOnInput []                           = runCLi []
+actOnInput ((=="TUI") . head -> True)   = runTUI
+actOnInput x                            = runCLi x
+
+
+
+
+
+
+
 
 
 
