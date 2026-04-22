3c1e70bddde11f35959a5a96bcab38a22083d8e3
Martin Elde Knutsen <maknu0536@uib.no>
2026-04-22 12:48:44 +0200

    Linet paranteser i printfuction

--- src/PrintFunctions.hs
+++ src/PrintFunctions.hs
@@ -1,25 +1,27 @@
 module PrintFunctions (printResults) where
 
-import TraverselsFunctions                   (FileInfomation(..)) 
+import TraverselsFunctions   (FileInfomation(..)) 
 
-import System.Posix.Terminal                 (queryTerminal)
-import System.Posix.IO                       (stdOutput)
-import TraversalSettings                     (convertToString)
+import System.Posix.Terminal (queryTerminal)
+import System.Posix.IO       (stdOutput)
+import TraversalSettings     (convertToString)
 
 
 import System.Console.ANSI.Codes (
         setSGRCode
-      , Color( Green)
+      , Color         (Green)
       , ColorIntensity(Vivid)
-      , ConsoleLayer(Foreground)
-      , SGR(Reset, SetColor)
+      , ConsoleLayer  (Foreground)
+      , SGR           (Reset, SetColor)
       )
+
+
 printResults :: [FileInfomation] -> IO ()
 printResults contents = do
     color <- coloriseFileIfTTY  
-    mapM_ (printFileInformation color)   contents 
+    mapM_ (printFileInformation color) contents 
 
--- | printer et Enkelt DirContent element
+-- | Printer et Enkelt DirContent element
 printFileInformation :: (String -> String) -> FileInfomation -> IO ()
 printFileInformation colorFunc fi = do
         case dirContent fi of
@@ -30,10 +32,14 @@ printFileInformation colorFunc fi = do
                 let path = fp <> ( '/' : colorFunc fn)
                 putStrLn path 
 
+
+-- | Printer et Enkelt DirContent element
 coloriseFileIfTTY :: IO (String -> String)
 coloriseFileIfTTY = do
     tty <- queryTerminal stdOutput
-    pure $ if tty then coloriseFile else id
+    pure $ if tty
+           then coloriseFile
+           else id
 
 coloriseFile :: String -> String
 coloriseFile rfp = setSGRCode [SetColor Foreground Vivid Green] <> rfp <> setSGRCode [Reset]
