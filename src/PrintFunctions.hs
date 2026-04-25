ea37a9bf0144b518b88f61cb25cef223a79ad6ed
Martin Elde Knutsen <maknu0536@uib.no>
2026-04-25 13:39:46 +0200

    Fikset slik at den bruker riktig get funksjon

--- src/PrintFunctions.hs
+++ src/PrintFunctions.hs
@@ -1,20 +1,17 @@
 module PrintFunctions (printResults) where
 
 import TraverselsFunctions   (FileInfomation(..)) 
-
 import System.Posix.Terminal (queryTerminal)
 import System.Posix.IO       (stdOutput)
 import TraversalSettings     (convertToString)
 
-
 import System.Console.ANSI.Codes (
         setSGRCode
-      , Color         (Green)
-      , ColorIntensity(Vivid)
-      , ConsoleLayer  (Foreground)
-      , SGR           (Reset, SetColor)
+      , Color          (Green)
+      , ColorIntensity (Vivid)
+      , ConsoleLayer   (Foreground)
+      , SGR            (Reset, SetColor)
       )
-
 
 
 -- | Printer all filene som er funnet 
@@ -24,11 +21,10 @@ printResults contents = do
     color <- coloriseFileIfTTY  
     mapM_ (printFileInformation color) contents  -- mapM_ siden den bare skal >> ikke >>= basicly
 
-
 -- | Printer et Enkelt FileInfomation element. Og bruker fargefunksjoen på bare filen
 printFileInformation :: (String -> String) -> FileInfomation -> IO ()
 printFileInformation colorFunc fi = do
-        case dirContent fi of
+        case fileNameInfo fi of
             Nothing -> pure ()
             Just dc  -> do
                 let fp   = convertToString . filePath $ fi
