a34ca3bff4af9fbef6afa296f9e6f91f6970ec96
Martin Elde Knutsen <maknu0536@uib.no>
2026-04-30 14:17:38 +0200

    Litt finpuss i main

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,7 @@ module Main (main) where
 
 import SearchTUI           (mainTUI)
 import ParseInput          (runParserIO)
-import System.Environment  (getArgs )
+import System.Environment  (getArgs)
 
 import TraverselsFunctions (treverseDirWithSettings)
 import PrintFunctions      (printResults)
@@ -14,21 +14,20 @@ main = do
 
 
 
+actOnInput :: [String] ->  IO()
+actOnInput []                           = runCLi []
+actOnInput ((=="TUI") . head -> True)   = runTUI
+actOnInput x                            = runCLi x
+
 runTUI :: IO ()
 runTUI = mainTUI
 
 runCLi :: [String] -> IO ()
 runCLi args = do
-     let inpString = unwords args
-     ss     <- runParserIO inpString
+     ss     <- runParserIO $ unwords args 
      output <- treverseDirWithSettings ss
      printResults output
 
-
-actOnInput :: [String] ->  IO()
-actOnInput []                           = runCLi []
-actOnInput ((=="TUI") . head -> True)   = runTUI
-actOnInput x                            = runCLi x
 
 
 
