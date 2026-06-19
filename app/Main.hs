4c625654c940535ba5a2b3e37f6076b7495bbeb3
Martin Elde Knutsen <maknu0536@uib.no>
2026-06-19 11:16:54 +0200

    Fjernet TUI

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,5 @@
 module Main (main) where
 
-import SearchTUI           (mainTUI)
 import ParseInput          (runParserIO)
 import System.Environment  (getArgs)
 
@@ -13,14 +12,10 @@ main = do
      actOnInput input
 
 
-
 actOnInput :: [String] ->  IO()
 actOnInput []                           = runCLi []
-actOnInput ((=="TUI") . head -> True)   = runTUI
 actOnInput x                            = runCLi x
 
-runTUI :: IO ()
-runTUI = mainTUI
 
 runCLi :: [String] -> IO ()
 runCLi args = do
